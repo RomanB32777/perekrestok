310dd8f31633180c5b61f294bab52fb9068e6bcb
Roman Bogdanovskiy <r.bogdanovskiy@onpoint.ru>
2022-10-25 20:46:38 +0300

    ru locale date picker

--- frontend/src/components/FormDatePicker/index.tsx
+++ frontend/src/components/FormDatePicker/index.tsx
@@ -1,4 +1,8 @@
 import { DatePicker } from "antd";
+
+import 'moment/locale/ru';
+import locale from 'antd/es/date-picker/locale/ru_RU';
+
 import "./styles.sass";
 
 const FormDatePicker = ({
@@ -6,12 +10,14 @@ const FormDatePicker = ({
   toFormat,
   placeholder,
   maxDate,
+  showToday,
   setValue,
 }: {
   value?: moment.Moment | undefined;
   toFormat?: string;
   placeholder?: string;
   maxDate?: moment.Moment;
+  showToday?: boolean;
   setValue: (date: moment.Moment | undefined) => void;
 }) => {
   return (
@@ -22,10 +28,10 @@ const FormDatePicker = ({
         bordered={false}
         onChange={(val) => (val ? setValue(val.utc()) : setValue(undefined))}
         placeholder={placeholder || "Выберите дату"}
-        disabledDate={
-          (d) => !d || d.isAfter(maxDate)
-        }
+        disabledDate={(d) => !d || d.isAfter(maxDate)}
         defaultPickerValue={maxDate}
+        locale={locale}
+        showToday={showToday}
       />
     </div>
   );
